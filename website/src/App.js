22d13c3f9cf8d94143c5b746815a4f9ac44d7164
harshitjain156 <92665993+harshitjain156@users.noreply.github.com>
2022-08-17 14:07:21 +0530

    file arranged

--- website/src/App.js
+++ website/src/App.js
@@ -1,6 +1,7 @@
 import data from "./data.json";
 
 import {useEffect,useState} from 'react';
+import Card from "./components/Card";
 
 function App() {
   const [theme, setTheme] = useState("light-theme");
@@ -25,31 +26,8 @@ function App() {
       </div>
       {data.data.map((portfolio) => (
         <div key={portfolio.portfolio_website}>
-        <a 
-          className="project"
-          href={portfolio.portfolio_website}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={
-              portfolio.portfolio_image ? portfolio.portfolio_image : "no image"
-            }
-            alt={
-              portfolio.portfolio_image ? portfolio.portfolio_image : "no image"
-            }
-          />
-          <div className="container">
-            <p className="personName">{portfolio.portfolio_name}</p>
-            <a
-              className="projectcode"
-              href={portfolio.portfolio_code}
-              target="_blank"
-              rel="noreferrer"
-            >Source Code</a>
-          </div>
-
-        </a>
+          <Card  name={portfolio.portfolio_name} website={portfolio.portfolio_website} image={portfolio.portfolio_image} code={portfolio.portfolio_code} />
+        
         </div>
       ))}
     </>
---
+++ website/src/components/Card.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+
+function Card(props) {
+  return (
+    <div>
+   
+        <a
+          className="project"
+          href={props.website}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={props.image}
+            alt={
+              props.image ? props.image : "no image"
+            }
+          />
+          <div className="container">
+            <p className="personName">{props.name}</p>
+            <a
+              className="projectcode"
+              href={props.code}
+              target="_blank"
+              rel="noreferrer"
+            >Source Code</a>
+          </div>
+
+        </a>
+    </div>
+  )
+}
+
+export default Card
+
+
+
+
