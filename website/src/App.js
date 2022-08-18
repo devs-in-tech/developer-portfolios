0c4233976e6333ba5992ee94a1beaf9ebea65959
Soham Chatterjee <sohamchatterjee2324@gmail.com>
2022-08-18 22:25:28 +0530

    Added header section for issue #49

--- website/src/App.js
+++ website/src/App.js
@@ -6,6 +6,11 @@ function App() {
       <div className="header">
         <h1>OpenSourceHub</h1>
       </div>
+      <div className = "about">
+        <h1>About this Project</h1>
+        <hr align="left" color="#61481c"></hr>
+        <p>Want to build a portfolio for yourself but could not get a satisfactory motivation? <br></br><br></br>This project brings together some awesome portfolio sites designed by developers to get you the right motivaiton. Click on the portfolio cards below and it will redirect you to that site. To view the code, click on the source code button.</p>
+      </div>
       {data.data.map((portfolio) => (
         <a
           className="project"
